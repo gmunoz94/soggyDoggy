edf0e58a8c4abdac2bf716608a6caa1cf413c2ab
gmunoz94 <g.munozii@icloud.com>
2021-11-15 18:34:39 -0600

    adding components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Linkbar from './components/Linkbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footbar from './components/Footbar';
+import { Col, Container, Row } from 'react-bootstrap';
+
 
 function App() {
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Linkbar />
+      <Container fluid>
+        <Row>
+          <Col>Placeholder</Col>
+          <Col>2 of 2</Col>
+        </Row>
+      </Container>
+      <Footbar />
     </div>
   );
 }
---
+++ src/components/Footbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+
+function Footbar() {
+
+    return(
+        <div className="footer">
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+                </Container>
+            </Navbar>
+        </div>
+    )
+};
+
+export default Footbar;
